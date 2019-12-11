a6c320861060a804eca5f621bf29f7bf7b63833a
Chen Rong <crong@google.com>
2019-12-11 11:32:39 -0800

    it's actually another type of binary search

--- LC0201-0400/LC240.java
+++ LC0201-0400/LC240.java
@@ -3,7 +3,7 @@
 @MEDIUM
 public class LC240 {
 
-  @DivideConquer
+  @BinarySearch
   public boolean searchMatrix(int[][] matrix, int target) {
     if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
       return false;
