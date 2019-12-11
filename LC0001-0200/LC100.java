98fe89237ccbf0f37a382d70c57dd880e8790b4c
Daniel Rong <crong@crong-macbookpro2.roam.corp.google.com>
2019-12-10 23:23:56 -0800

    LC100 & LC1120

---
+++ LC0001-0200/LC100.java
@@ -0,0 +1,24 @@
+// Balanced Binary Tree
+
+@EASY
+public class LC100 {
+  private boolean balanced = true;
+
+  @DivideConquer
+  public boolean isBalanced(TreeNode root) {
+    treeHeight(root);
+
+    return balanced;
+  }
+
+  private int treeHeight(TreeNode node) {
+    if (!balanced || node == null) return 0;
+
+    int left = treeHeight(node.left);
+    int right = treeHeight(node.right);
+
+    balanced = Math.abs(left - right) > 1 ? false : balanced;
+
+    return Math.max(left, right) + 1;
+  }
+}
