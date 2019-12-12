577db7a228ce9105645c89c9843a2625165f0f8d
Daniel Rong <crong@crong-macbookpro2.roam.corp.google.com>
2019-12-11 21:43:25 -0800

    Validate Binary Search Tree

---
+++ LC0001-0200/LC98.java
@@ -0,0 +1,22 @@
+// Validate Binary Search Tree
+
+@MEDIUM
+class LC98 {
+
+  @DivideConquer
+  @TopDown
+  public boolean isValidBST(TreeNode root) {
+    return check(root, Long.MAX_VALUE, Long.MIN_VALUE);
+  }
+
+  private boolean check(TreeNode node, long upperBound, long lowerBound) {
+    if (node == null) return true;
+
+    if (node.val <= lowerBound || node.val >= upperBound) return false;
+
+    boolean left = check(node.left, node.val, lowerBound);
+    boolean right = check(node.right, upperBound, node.val);
+
+    return left && right;
+  }
+}
