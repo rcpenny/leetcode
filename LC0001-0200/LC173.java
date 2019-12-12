af2670209cf82fe2604d3b5a717be852d529a336
Daniel Rong <crong@crong-macbookpro2.roam.corp.google.com>
2019-12-11 20:25:30 -0800

    Binary Search Tree Iterator

---
+++ LC0001-0200/LC173.java
@@ -0,0 +1,34 @@
+//  Binary Search Tree Iterator
+
+@MEDIUM
+class BSTIterator {
+
+  Stack<TreeNode> stack;
+
+  @DataStructStack
+  public BSTIterator(TreeNode root) {
+    stack = new Stack<>();
+    pushToStack(root, stack);
+  }
+  
+  /** @return the next smallest number */
+  public int next() {
+    TreeNode peek = stack.pop();
+    if (peek.right != null) {
+      pushToStack(peek.right, stack);
+    }
+    return peek.val;
+  }
+  
+  /** @return whether we have a next smallest number */
+  public boolean hasNext() {
+    return !stack.isEmpty();
+  }
+
+  private void pushToStack(TreeNode node, Stack<TreeNode> stack) {
+    while (node != null) {
+      stack.push(node);
+      node = node.left;
+    }
+  }
+}
