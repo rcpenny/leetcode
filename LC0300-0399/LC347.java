17c37b1dd9cff70ec12792974dff64a296eff858
Daniel Rong <crong@crong-macbookpro2.roam.corp.google.com>
2019-12-13 00:56:13 -0800

    Top K Frequent Elements

---
+++ LC0300-0399/LC347.java
@@ -0,0 +1,28 @@
+// Top K Frequent Elements
+
+@MEDIUM
+public class LC347 {
+
+  @Heap
+  @HashTable
+  public List<Integer> topKFrequent(int[] nums, int k) {
+
+    Map<Integer, Integer> map = new HashMap<>();
+    for (int num : nums) {
+      map.put(num, map.getOrDefault(num, 0) + 1);
+    }
+
+    PriorityQueue<Map.Entry<Integer, Integer>> heap = new PriorityQueue<>((a, b) -> {
+      return b.getValue() - a.getValue();
+    });
+
+    for (Map.Entry<Integer, Integer> e : map.entrySet()) {
+      heap.offer(e);
+    }
+
+    List<Integer> result = new ArrayList<>();
+    while (k-- > 0) result.add(heap.poll().getKey());
+    
+    return result;
+  }
+}
