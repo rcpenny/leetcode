c74109763d07f29230ea0cd243f817900df411e0
Chen Rong <crong@google.com>
2019-12-13 11:52:56 -0800

    Jump Game DP, greedy needed

--- LC0001-0099/LC55.java
+++ LC0001-0099/LC55.java
@@ -5,11 +5,25 @@ public class LC55 {
 
 	@Greedy
 	public boolean canJump(int[] nums) {
-        
+
 	}
 
 	@DynamicProgramming
 	public boolean canJump1(int[] nums) {
-        
+		int n = nums.length;
+
+		boolean[] f = new boolean[n];
+		f[0] = true;
+
+		for (int i = 1; i < n; i++) {
+			for (int j = i - 1; j >= 0; j--) {
+				if (f[j] && j + nums[j] >= i) {
+					f[i] = true;
+					break;
+				}
+			}
+		}
+
+		return f[n - 1];
 	}
 }
