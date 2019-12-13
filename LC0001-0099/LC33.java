008f652ca6362d3080fd426ac444b1c8294a1cf6
Daniel Rong <crong@crong-macbookpro2.roam.corp.google.com>
2019-12-13 00:22:33 -0800

    Search in Rotated Sorted Array

---
+++ LC0001-0099/LC33.java
@@ -0,0 +1,35 @@
+// Search in Rotated Sorted Array
+
+@MEDIUM
+public class LC33 {
+  
+  @BinarySearch
+  public int search(int[] nums, int target) {
+    if (nums == null || nums.length == 0) return -1;
+
+    int start = 0;
+    int end = nums.length - 1;
+
+    int tail = nums[end];
+
+    while (start + 1 < end) {
+      int mid = start + (end - start) / 2;
+      
+      if (nums[mid] == target) return mid;
+
+      if (target > tail) {
+        if (nums[mid] > target || nums[mid] <= tail) end = mid;
+        else start = mid;
+      }
+
+      else {
+        if (nums[mid] < target || nums[mid] > tail) start = mid;
+        else end = mid;
+      }
+    }
+
+    if (nums[start] == target) return start;
+    if (nums[end] == target) return end;
+    return -1;
+  }
+}
