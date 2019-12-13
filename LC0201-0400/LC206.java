5dd0da08a14c0fa2832af4effd2eabb05de9b9a7
Daniel Rong <crong@crong-macbookpro2.roam.corp.google.com>
2019-12-12 18:16:02 -0800

    ss

---
+++ LC0201-0400/LC206.java
@@ -0,0 +1,22 @@
+// Reverse Linked List
+
+@EASY
+public class LC206 {
+  
+  @LinkedList
+  public ListNode reverseList(ListNode head) {
+    ListNode pre = null;
+    ListNode cur = head;
+    ListNode nex = null;
+
+    while (cur != null) {
+      nex = cur.next;
+      cur.next = pre;
+
+      pre = cur;
+      cur = nex;
+    }
+
+    return pre; 
+  }
+}
---
+++ LC0201-0400/LC208.java
@@ -0,0 +1,66 @@
+// Implement Trie (Prefix Tree)
+
+@MEDIUM
+@Trie
+public class LC208 {}
+
+public class Trie {
+
+  TrieNode root;
+
+  public Trie() {
+    root = new TrieNode(' ');
+  }
+
+  public void insert(String word) {
+    TrieNode node = root;
+    for (int i = 0; i < word.length(); i++) {
+      char c = word.charAt(i);
+      if (node.children[c - 'a'] == null) {
+        node.children[c - 'a'] = new TrieNode(c);
+      }
+      node = node.children[c - 'a'];
+    }
+    node.isWord = true;
+  }
+
+  public boolean search(String word) {
+    TrieNode node = root;
+
+    for (int i = 0; i < word.length(); i++) {
+      char c = word.charAt(i);
+      if (node.children[c - 'a'] == null) {
+        return false;
+      }
+      node = node.children[c - 'a'];
+    }
+
+    return node.isWord;
+  }
+  
+  public boolean startsWith(String prefix) {
+    TrieNode node = root;
+
+    for (int i = 0; i < prefix.length(); i++) {
+      char c = prefix.charAt(i);
+      if (node.children[c - 'a'] == null) {
+        return false;
+      }
+      node = node.children[c - 'a'];
+    }
+
+    return true;
+  }
+
+  class TrieNode {
+    char c;
+    boolean isWord;
+    TrieNode[] children;
+
+    TrieNode(char c) {
+      this.c = c;
+      this.isWord = false;
+      this.children = new TrieNode[26];
+    }
+  }
+}
