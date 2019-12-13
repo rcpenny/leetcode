53d34186e33fd6a2fa2039994e7f5a8ef0cadd9b
Chen Rong <crong@google.com>
2019-12-13 13:18:19 -0800

    Word Break

---
+++ LC0100-0199/LC139.java
@@ -0,0 +1,28 @@
+import java.util.Set;
+
+// Word Break
+
+@MEDIUM
+public class LC139 {
+
+	@DynamicProgramming
+	public boolean wordBreak(String s, List<String> wordDict) {
+		Set<String> dict =new HashSet<>(wordDict);
+
+		int n = s.length();
+
+		boolean[] f = new boolean[n + 1];
+		f[0] = true;
+
+		for (int i = 1; i <= n; i++) {
+			for (int j = 0; j < i; j++) {
+				if (f[j] && dict.contains(s.substring(j, i))) {
+					f[i] = true;
+					break;
+				}
+			}
+		}
+
+		return f[n];
+	}
+}
