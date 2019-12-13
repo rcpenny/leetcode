5cc503b87fd967b77c3045da6db2d7463050f2de
Chen Rong <crong@google.com>
2019-12-13 10:23:31 -0800

    Climbing Stairs

--- LC0001-0099/LC70.java
+++ LC0001-0099/LC70.java
@@ -1,11 +1,40 @@
 // Climbing Stairs
 
+// n is positive integer
+
 @EASY
 public class LC70 {
 
 	@DynamicProgramming
-	@Coordiante
 	public int climbStairs(int n) {
-        
+		if (n == 1) return 1;
+
+		int[] f = new int[n + 1];
+
+		f[1] = 1;
+		f[2] = 2;
+
+		for (int i = 3; i <= n; i++) {
+			f[i] = f[i - 1] + f[i - 2];
+		}
+
+		return f[n];
+	}
+
+	// O(1) space
+	public int climbStairs2(int n) {
+		if (n <= 2) return n;
+
+		int first = 1;
+		int second = 2;
+		int third = first + second;
+
+		for (int i = 3; i <= n; i++) {
+			third = first + second;
+			first = second;
+			second = third;
+		}
+
+		return third;
 	}
 }
