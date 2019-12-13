8cc57d43886224f4dc5e093d13583e7f6e32829d
Daniel Rong <crong@crong-macbookpro2.roam.corp.google.com>
2019-12-13 01:09:09 -0800

    Coin Change

---
+++ LC0300-0399/LC322.java
@@ -0,0 +1,28 @@
+// Coin Change
+
+@MEDIUM
+public class LC322 {
+  
+  @DynamicProgramming
+  @Sequence
+  public int coinChange(int[] coins, int amount) {
+    Arrays.sort(coins);
+
+    int[] f = new int[amount + 1];
+    f[0] = 0;
+
+    int i, j;
+
+    for (i = 1; i <= amount; i++) {
+      f[i] = Integer.MAX_VALUE;
+      for (j = 0; j < coins.length; j++) {
+        int comp = i - coins[j];
+        if (comp >= 0 && f[comp] != Integer.MAX_VALUE) {
+          f[i] = Math.min(f[i], f[comp] + 1);
+        }
+      }
+    }
+
+    return f[amount] == Integer.MAX_VALUE ? -1 : f[amount];
+  }
+}
