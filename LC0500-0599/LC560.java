4dbe04260a8086c44d7b4328f64d1ba1930956ef
Chen Rong <crong@google.com>
2019-12-14 13:54:26 -0800

    Subarray Sum Equals K

---
+++ LC0500-0599/LC560.java
@@ -0,0 +1,34 @@
+// Subarray Sum Equals K
+
+@MEDIUM
+public class LC560 {
+
+	@PrefixSum
+	@HashTable
+	public int subarraySum(int[] nums, int k) {
+		if (nums == null || nums.length == 0) return 0;
+
+		int count = 0;
+		int n = nums.length;
+		
+		// prefix_sum -> freq
+		Map<Integer, Integer> map = new HashMap<>();
+
+		int[] prefix = new int[n + 1];
+		prefix[0] = 0;
+		map.put(0, 1);
+
+		for (int i = 1; i <= n; i++) {
+			prefix[i] = prefix[i - 1] + nums[i - 1];
+
+			int comp = prefix[i] - k;
+			
+			if (map.containsKey(comp)) {
+				count += map.get(comp);
+			}
+			map.put(prefix[i], map.getOrDefault(prefix[i], 0) + 1);
+		}
+
+		return count;
+	}
+}
