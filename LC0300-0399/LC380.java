d1308295f85bddf3f7e8e4d15e5196c53c7ff475
Chen Rong <crong@google.com>
2019-12-15 14:40:24 -0800

    Insert Delete GetRandom O(1)

---
+++ LC0300-0399/LC380.java
@@ -0,0 +1,52 @@
+// Insert Delete GetRandom O(1)
+
+@MEDIUM
+public class LC380 {}
+
+@HashTable
+@DesignProblem
+class RandomizedSet {
+
+	List<Integer> list;
+	Map<Integer, Integer> map;
+	Random rand;
+
+	public RandomizedSet() {
+		list = new ArrayList<>();
+		map = new HashMap<>();
+		rand = new Random();
+	}
+	
+	/** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
+	public boolean insert(int val) {
+		if (map.containsKey(val)) return false;
+
+		list.add(val);
+		map.put(val, list.size() - 1);
+		return true;
+	}
+
+	/** Removes a value from the set. Returns true if the set contained the specified element. */
+	public boolean remove(int val) {
+		if (!map.containsKey(val)) return false;
+
+		int lastIndex = list.size() - 1;
+		int lastVal = list.get(lastIndex);
+
+		int valIndex = map.get(val);
+		
+		map.put(lastVal, valIndex);
+		list.set(valIndex, lastVal);
+
+		list.remove(lastIndex);
+		map.remove(val);
+
+		return true;
+	}
+	
+	/** Get a random element from the set. */
+	public int getRandom() {
+		int index = rand.nextInt(list.size());
+		return list.get(index);
+	}
+}
