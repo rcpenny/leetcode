1c7f379a694f772bdd5411d76aff1bf8cf84d130
Chen Rong <crong@google.com>
2019-12-17 15:33:08 -0800

    CombinationIterator

---
+++ LC1200-1299/LC1286.java
@@ -0,0 +1,20 @@
+// Iterator for Combination
+
+@MEDIUM
+public class LC1286 {}
+
+@IteratorDesign
+class CombinationIterator {
+
+	public CombinationIterator(String characters, int combinationLength) {
+
+	}
+	
+	public String next() {
+		
+	}
+	
+	public boolean hasNext() {
+
+	}
+}
