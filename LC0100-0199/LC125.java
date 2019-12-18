204111a50ff4afa9a159e847970e2cf1150dd0a6
Daniel Rong <crong@crong-macbookpro2.roam.corp.google.com>
2019-12-17 20:59:42 -0800

    Valid Palindrome

---
+++ LC0100-0199/LC125.java
@@ -0,0 +1,39 @@
+// Valid Palindrome
+
+@EASY
+class LC125 {
+
+  @TwoPointers
+  public boolean isPalindrome(String s) {
+    if (s.length() == 0) return true;
+
+    int left = 0;
+    int right = s.length() - 1;
+    
+    char[] c = s.toCharArray();
+
+    while (left < right) {
+      
+      while (left < right && !validCharacter(c[left])) {
+        left++;
+      }
+
+      while (left < right && !validCharacter(c[right])) {
+        right--;
+      }
+
+      if (Character.toLowerCase(c[left]) != Character.toLowerCase(c[right])) {
+        return false;
+      }
+
+      left++;
+      right--;
+    }
+
+    return true;
+  }
+
+  private boolean validCharacter(char c) {
+    return Character.isLetter(c) || Character.isDigit(c);
+  }
+}
