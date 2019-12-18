b25b761efae6fb6a24e4ef07d73dfc06d8aed831
Chen Rong <crong@google.com>
2019-12-18 10:43:53 -0800

    Unique Email Addresses

---
+++ LC0900-0999/LC929.java
@@ -0,0 +1,24 @@
+// Unique Email Addresses
+
+@EASY
+public class LC929 {
+
+	@String
+	public int numUniqueEmails(String[] emails) {
+		Set<String> seen = new HashSet<>();
+		
+		for (String email : emails) {
+			int i = email.indexOf('@');
+			String alias = email.substring(0, i);
+			String domain = email.substring(i);
+
+			if (alias.contains("+")) {
+				alias = alias.substring(0, alias.indexOf('+'));
+			}
+			alias = alias.replaceAll(".", "");
+			seen.add(alias + domain);
+		}
+
+		return seen.size();
+	}
+}
