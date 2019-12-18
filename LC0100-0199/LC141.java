51a49670a7e91c7e2d1d055762f2d018f6875293
Daniel Rong <crong@crong-macbookpro2.roam.corp.google.com>
2019-12-17 21:07:20 -0800

    Linked List Cycle

---
+++ LC0100-0199/LC141.java
@@ -0,0 +1,23 @@
+// Linked List Cycle
+
+@EASY
+public class LC141 {
+
+  @TwoPointers
+  @LinkedList
+  public boolean hasCycle(ListNode head) {
+    ListNode fast = head;
+    ListNode slow = head;
+
+    while (fast != null && fast.next != null) {
+      fast = fast.next.next;
+      slow = slow.next;
+
+      if (fast == slow) return true;
+    }
+
+    return false;
+  }
+}
+
+
