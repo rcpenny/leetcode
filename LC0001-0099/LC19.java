c49121f28a60fa47ff3c16750114dca080a01c76
Chen Rong <crong@google.com>
2019-12-18 11:37:15 -0800

    Remove Nth Node From End of List

---
+++ LC0001-0099/LC19.java
@@ -0,0 +1,30 @@
+// Remove Nth Node From End of List
+
+@MEDIUM
+public class LC19 {
+
+	@LinkedList
+	@TwoPointers
+	public ListNode removeNthFromEnd(ListNode head, int n) {
+		ListNode dummy = new ListNode(0);
+		dummy.next = head;
+
+		ListNode fast = dummy;
+		ListNode slow = dummy;
+
+		for (int i = 0; i <= n; i++) {
+			fast = fast.next;
+
+			if (fast == null) return head.next;
+		}
+
+		while (fast != null) {
+			fast = fast.next;
+			slow = slow.next;
+		}
+
+		slow.next = slow.next.next;
+
+		return dummy.next;
+	}
+}
