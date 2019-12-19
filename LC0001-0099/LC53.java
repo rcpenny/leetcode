af31954e2d09244efc493016fda171202fad6cd0
Daniel Rong <crong@crong-macbookpro2.roam.corp.google.com>
2019-12-18 21:55:00 -0800

    Maximum Subarray

---
+++ LC0001-0099/LC53.java
@@ -0,0 +1,20 @@
+// Maximum Subarray
+
+@EASY
+public class LC53 {
+
+  @DynamicProgramming
+  public int maxSubArray(int[] nums) {
+    int prefix = 0;
+    int curMin = 0;
+    int max = Integer.MIN_VALUE;
+
+    for (int i = 0; i < nums.length; i++) {
+      prefix += nums[i];
+      max = Math.max(max, prefix - curMin);
+      curMin = Math.min(curMin, prefix);
+    }
+
+    return max;
+  }
+}
