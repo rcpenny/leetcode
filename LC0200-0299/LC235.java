0a8a944f2cadd45be4351672e0712c468b112b16
Daniel Rong <crong@crong-macbookpro2.roam.corp.google.com>
2019-12-25 18:00:13 -0800

    Lowest Common Ancestor of a Binary Search Tree

--- LC0200-0299/LC235.java
+++ LC0200-0299/LC235.java
@@ -4,8 +4,16 @@
 @Microsoft
 class LC235 {
 
-  @
+  @BST  
   public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-      
+    if (root.val > p.val && root.val > q.val) {
+      return lowestCommonAncestor(root.left, p, q);
+    }
+
+    if (root.val < p.val && root.val < q.val) {
+      return lowestCommonAncestor(root.right, p, q);
+    }
+
+    return root;
   }
 }
