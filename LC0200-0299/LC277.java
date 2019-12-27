51a86ee67a9e9821d47d32fb23d4e1f40a6ea0c7
Daniel Rong <crong@crong-macbookpro2.roam.corp.google.com>
2019-12-27 11:17:39 -0800

    Find the Celebrity

---
+++ LC0200-0299/LC277.java
@@ -0,0 +1,26 @@
+// Find the Celebrity
+
+@MEDIUM
+@Microsoft
+public class LC277 extends Relation {
+
+  @Array
+  @Graph
+  public int findCelebrity(int n) {
+    int candidate = 0;
+
+    for (int i = 1; i < n; i++) {
+      if (knows(candidate, i)) {
+        candidate = i;
+      }
+    }
+
+    for (int i = 0; i < n; i++) {
+      if (i != candidate && (knows(candidate, i) || !knows(i, candidate))) {
+        return -1;
+      }
+    }
+
+    return candidate;
+  }
+}
