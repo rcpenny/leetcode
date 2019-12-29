d8212dac93e3a6842e0ab85d86d4e691d3cd602d
Daniel Rong <crong@crong-macbookpro2.roam.corp.google.com>
2019-12-28 16:38:17 -0800

    Trapping Rain Water

--- LC0001-0099/LC42.java
+++ LC0001-0099/LC42.java
@@ -6,6 +6,28 @@ public class LC42 {
 
   @TwoPointers
   public int trap(int[] height) {
-      
+    int rain = 0;
+
+    int left = 0;
+    int right = height.length - 1;
+
+    int leftMax = 0;
+    int rightMax = 0;
+
+    while (left < right) {
+
+      if (height[left] < height[right]) {
+        if (height[left] < leftMax) rain += leftMax - height[left];
+        else leftMax = height[left];
+        left++;
+        continue;
+      }
+
+      if (height[right] < rightMax) rain += rightMax - height[right];
+      else rightMax = height[right];
+      right--;
+    }
+
+    return rain;
   }
 }
