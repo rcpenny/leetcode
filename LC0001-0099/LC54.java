6dc6678cb1232dd61b53d2d9e10a57f4bddc153d
Daniel Rong <crong@crong-macbookpro2.roam.corp.google.com>
2019-12-28 22:39:24 -0800

    Spiral Matrix

---
+++ LC0001-0099/LC54.java
@@ -0,0 +1,52 @@
+// Spiral Matrix
+
+@MEDIUM
+@Microsoft
+public class LC54 {
+
+  @Array
+  public List<Integer> spiralOrder(int[][] matrix) {
+    List<Integer> result = new ArrayList<>();
+
+    if (matrix == null || matrix.length == 0) return result;
+
+    int m = matrix.length;
+    int n = matrix[0].length;
+
+    int x = 0;
+    int y = 0;
+
+    int i = 0;
+
+    while (m > 1 && n > 1) {
+      for (i = 1; i < n; i++) {
+        result.add(matrix[x][y++]);
+      }
+
+      for (i = 1; i < m; i++) {
+        result.add(matrix[x++][y]);
+      }
+
+      for (i = 1; i < n; i++) {
+        result.add(matrix[x][y--]);
+      }
+
+      for (i = 1; i < m; i++) {
+        result.add(matrix[x--][y]);
+      }
+
+      x++;
+      y++;
+      m -= 2;
+      n -= 2;
+    }
+
+    if (m == 1) {
+      for (i = 0; i < n; i++) result.add(matrix[x][y++]);
+    } else if (n == 1) {
+      for (i = 0; i < m; i++) result.add(matrix[x++][y]);
+    }
+
+    return result;
+  }
+}
