80ae543c2bd1e43ba4baa4418e8f98f368d57baf
Daniel Rong <crong@crong-macbookpro2.roam.corp.google.com>
2019-12-29 20:10:24 -0800

    Excel Sheet Column Number

---
+++ LC0100-0199/LC171.java
@@ -0,0 +1,18 @@
+// Excel Sheet Column Number
+
+@EASY
+@Microsoft
+class LC171 {
+
+  @Math
+  public int titleToNumber(String s) {
+    char[] ch = s.toCharArray();
+    int num = 0;
+
+    for (char c : ch) {
+      num = num * 26 + (c - 'A') + 1;
+    }
+
+    return num;
+  }
+}
