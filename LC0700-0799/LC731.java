05ac8a795e846b878997d280ff582ad345ae4e09
Daniel Rong <crong@crong-macbookpro2.roam.corp.google.com>
2020-01-02 00:48:55 -0800

    My Calendar II

---
+++ LC0700-0799/LC731.java
@@ -0,0 +1,15 @@
+// My Calendar II
+
+public class 731 {}
+
+@MEDIUM
+class MyCalendarTwo {
+
+  public MyCalendarTwo() {
+      
+  }
+  
+  public boolean book(int start, int end) {
+      
+  }
+}
