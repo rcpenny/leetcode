6a671bbe3aed72c13ff363a1d72d7a28a42b75ad
Daniel Rong <crong@crong-macbookpro2.roam.corp.google.com>
2020-01-01 19:33:22 -0800

    Plus One

---
+++ LC0001-0099/LC66.java
@@ -0,0 +1,30 @@
+// Plus One
+
+@EASY
+@Google
+public class LC66 {
+
+  @Array
+  public int[] plusOne(int[] digits) {
+    boolean flag = true;
+
+    int n = digits.length;
+    int[] result = new int[n];
+    int carry = 1;
+
+    for (int i = n - 1; i >= 0; i--) {
+      result[i] = (digits[i] + carry) % 10;
+      carry = (digits[i] + carry) / 10;
+      
+      if (digits[i] != 9) flag = false;
+    }
+
+    if (flag) {
+      int[] spec = new int[n + 1];
+      spec[0] = 1;
+      return spec;
+    }
+
+    return result;
+  }
+}
