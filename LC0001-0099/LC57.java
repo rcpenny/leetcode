b212bb85dd0fd0b4656565dceafe73601417fbfe
Daniel Rong <crong@crong-macbookpro2.roam.corp.google.com>
2020-01-01 21:12:12 -0800

    Insert Interval

---
+++ LC0001-0099/LC57.java
@@ -0,0 +1,32 @@
+// Insert Interval
+
+@HARD
+class LC57 {
+
+  @Array
+  public List<Interval> insert(List<Interval> intervals, Interval newInterval) {
+		List<Interval> result = new ArrayList<>();
+
+		int insertIndex = 0;
+ 
+		for (Interval in : intervals) {
+			if (in.end < newInterval.start) {
+				result.add(in);
+				insertIndex++;
+				continue;
+			}
+
+			if (in.start > newInterval.end) {
+				result.add(in);
+				continue;
+			}
+
+			newInterval.start = Math.min(newInterval.start, in.start);
+			newInterval.end = Math.max(newInterval.end, in.end);
+		}
+
+		result.add(insertIndex, newInterval);
+
+		return result;
+  }
+}
