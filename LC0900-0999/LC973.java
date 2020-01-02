e857cbc8d83e58f7268b20fa1467af399643923c
Daniel Rong <crong@crong-macbookpro2.roam.corp.google.com>
2020-01-01 20:03:33 -0800

    K Closest Points to Origin

--- LC0900-0999/LC973.java
+++ LC0900-0999/LC973.java
@@ -1,10 +1,29 @@
 // K Closest Points to Origin
 
 @MEDIUM
+@Google
 public class LC973 {
 
 	@Heap
 	public int[][] kClosest(int[][] points, int K) {
-  
+		int[][] result = new int[K][2];
+
+		PriorityQueue<int[]> minheap = new PriorityQueue<>((a, b) -> {
+			return getDistance(a) - getDistance(b) < 0 ? -1 : 1;
+		});
+
+		for (int[] p : points) {
+			minheap.offer(p);
+		}
+
+		for (int i = 0; i < K; i++) {
+			result[i] = minheap.poll();
+		}
+		
+		return result;
+	}
+
+	private double getDistance(int[] points) {
+		return Math.sqrt(points[0] * points[0] + points[1] * points[1]);
 	}
 }
