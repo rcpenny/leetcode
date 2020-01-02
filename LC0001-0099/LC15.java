da6f1cf01aa55638e69efe3c3787debcd988b828
Daniel Rong <crong@crong-macbookpro2.roam.corp.google.com>
2020-01-01 20:35:46 -0800

    3Sum

---
+++ LC0001-0099/LC15.java
@@ -0,0 +1,43 @@
+// 3Sum
+
+@MEDIUM
+@Google
+class LC15 {
+
+  @TwoPointers
+  public List<List<Integer>> threeSum(int[] nums) {
+    List<List<Integer>> result = new  ArrayList<>();
+    if (numbers == null || numbers.length < 3) return result;
+
+    Arrays.sort(nums);
+
+    for (int i = 0; i < nums.length - 2; i++) {
+      if (i > 0 && nums[i] == nums[i - 1]) continue;
+      twoSum(nums, i, result);
+    }
+
+    return result;
+  }
+
+  private void twoSum(int[] nums, int i, List<List<Integer>> result) {
+    int target = 0 - nums[i];
+    int left = i + 1;
+    int right = nums.length - 1;
+
+    while (left < right) {
+      int tmp = nums[left] + nums[right];
+      if (target == tmp) {
+        List<Integer> triple = Arrays.asList(nums[i], nums[left], nums[right]);
+        result.add(triple);
+        left++;
+        right--;
+        while (left < right && nums[left] == nums[left - 1]) left++;
+        while (left < right && nums[right] == nums[right + 1]) right--;
+      } else if (tmp < target) {
+        left++;
+      } else if (tmp > target) {
+        right--;
+      }
+    }
+  }
+}
