066281c6a3dfb4a7d6eb712c714d3caa4bace430
Daniel Rong <crong@crong-macbookpro2.roam.corp.google.com>
2020-01-02 19:58:55 -0800

    Next Permutation

---
+++ LC0001-0099/LC31.java
@@ -0,0 +1,36 @@
+// Next Permutation
+
+@MEDIUM
+public class LC31 {
+
+  @Array
+  public void nextPermutation(int[] nums) {
+    int i = nums.length - 2;
+    while (i >= 0 && nums[i + 1] <= nums[i]) {
+      i--;
+    }
+
+    if (i >= 0) {
+      int j = nums.length - 1;
+      while (j >= 0 && nums[j] <= nums[i]) {
+        j--;
+      }
+      swap(nums, i, j);
+    }
+    reverse(nums, i + 1);
+  }
+
+  private void reverse(int[] nums, int start) {
+    int left = start;
+    int right = nums.length - 1;
+    while (left < right) {
+      swap(nums, left++, right--);
+    }
+  }
+
+  private void swap(int[] nums, int i, int j) {
+    int tmp = nums[i];
+    nums[i] = nums[j];
+    nums[j] = tmp;
+  }
+}
