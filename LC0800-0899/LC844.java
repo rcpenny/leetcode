27658b27c7dc8805255fe4a5075f7b0c3be8988c
Daniel Rong <crong@crong-macbookpro2.roam.corp.google.com>
2020-01-02 23:19:32 -0800

    Backspace String Compare

---
+++ LC0800-0899/LC844.java
@@ -0,0 +1,35 @@
+// Backspace String Compare
+
+@EASY
+public class LC844 {
+  
+  @Stack
+  public boolean backspaceCompare(String S, String T) {
+    String s = getText(S);
+    String t = getText(T);
+
+    if (s == null && t == null) return true;
+    if (s == null || t == null) return false;
+
+    return s.equals(t);
+  }
+
+  private String getText(String s) {
+    Stack<Character> stack = new Stack<>();
+    
+    for (char c : s.toCharArray()) {
+      if (Character.isLetter(c)) {
+        stack.push(c);
+      } else if (!stack.isEmpty()) {
+        stack.pop();
+      }
+    }
+
+    StringBuilder sb = new StringBuilder();
+    while (!stack.isEmpty()) {
+      sb.append(stack.pop());
+    }
+
+    return sb.toString();
+  }
+}
