dbff5c5a2aaa77f2df36555761f0aebe29bf5e94
Daniel Rong <crong@crong-macbookpro2.roam.corp.google.com>
2020-01-02 22:42:54 -0800

    Minimum Window Substring

---
+++ LC0001-0099/LC76.java
@@ -0,0 +1,43 @@
+// Minimum Window Substring
+
+@HARD
+public class LC76 {
+  int[] letters = new int[256];
+
+  @TwoPointers
+  public String minWindow(String source , String target) {
+		for (int i = 0; i < target.length(); i++) {
+      letters[target.charAt(i)]++;
+    }
+
+		int slow = 0;
+		int fast = 0;
+		int min_length = Integer.MAX_VALUE;
+		
+		String min_window = "";
+			
+    for (slow = 0; slow < source.length(); slow++) {
+			
+      while (fast < source.length() && !isAllFound()) {
+        letters[source.charAt(fast)]--;
+        fast++;
+      }
+			
+      if (fast - slow < min_length && isAllFound()) {
+        min_length = fast - slow;
+        min_window = source.substring(slow, fast);
+      }
+
+			letters[source.charAt(slow)]++;
+    }
+
+    return min_window;     
+  }
+  
+  private boolean isAllFound() {
+    for (int i = 0; i < letters.length; i++) {
+			if (letters[i] > 0) return false;
+		}
+    return true;
+  }
+}
