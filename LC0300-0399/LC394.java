d419eeb382807d5ef5fc164826f5078691f1bc2a
Daniel Rong <crong@crong-macbookpro2.roam.corp.google.com>
2020-01-02 22:35:32 -0800

    Decode String

---
+++ LC0300-0399/LC394.java
@@ -0,0 +1,41 @@
+// Decode String
+
+@MEDIUM
+public class LC394 {
+
+  @Stack
+  public String decodeString(String s) {
+		Stack<Object> stack = new Stack<>();
+		int number = 0;
+
+		for (char c : s.toCharArray()) {
+			if (Character.isDigit(c)) {
+				number = number * 10 + (c - '0');
+			} else if (c == '[') {
+				stack.push(Integer.valueOf(number));
+				number = 0;
+			} else if (c == ']') {
+				String newStr = popStack(stack);
+				Integer count = (Integer) stack.pop();
+				for (int i = 0; i < count; i++) {
+					stack.push(newStr);
+				}
+			} else {
+				stack.push(String.valueOf(c));
+			}
+		}
+		return popStack(stack);
+	}
+
+	private String popStack(Stack<Object> stack) {
+		Stack<String> buffer = new Stack<>();
+
+		while (!stack.isEmpty() && (stack.peek() instanceof String)) {
+			buffer.push((String) stack.pop());
+		}
+
+		StringBuilder sb = new StringBuilder();
+		while (!buffer.isEmpty()) sb.append(buffer.pop());
+		return sb.toString();
+	}
+}
