da4a3be00c7469814d3c04085e30f397bc51f9e3
Daniel Rong <crong@crong-macbookpro2.roam.corp.google.com>
2020-01-07 13:11:05 -0800

    Minimum Size Subarray Sum

---
+++ LC0200-0299/LC209.java
@@ -0,0 +1,24 @@
+// Minimum Size Subarray Sum
+
+@MEDIUM
+public class LC209 {
+
+  @TwoPointers
+  public int minSubArrayLen(int s, int[] nums) {
+    int n = nums.length;
+    int sum = 0;
+    int len = Integer.MAX_VALUE;
+    
+    int slow = 0;
+
+    for (int fast = 0; fast < n; fast++) {
+      sum += nums[fast];
+      while (sum >= s) {
+        len = Math.min(len, fast - slow + 1);
+        sum -= nums[slow++];
+      }
+    }
+
+    return len != Integer.MAX_VALUE ? len : 0;
+  }
+}
