100ba1a89fc433c02f6749b428819e04899ec2f2
Daniel Rong <crong@crong-macbookpro2.roam.corp.google.com>
2020-01-10 13:24:02 -0800

    Pow(x, n)

---
+++ LC0001-0099/LC50.java
@@ -0,0 +1,25 @@
+// Pow(x, n)
+
+@MEDIUM
+public class LC50 {
+
+  @BinarySearch
+  public double myPow(double x, int n) {
+    if (x == 0.0) return 0.0;
+    if (n == 0) return 1.0;
+
+    if (n < 0) x = 1 / x;
+    n = 0 - n;
+
+    double result = 1.0;
+    double product = x;
+
+    while (n != 0) {
+      if (n % 2 != 0) result *= product;
+      n /= 2;
+      product *= product;
+    }
+
+    return result;
+  }
+}
