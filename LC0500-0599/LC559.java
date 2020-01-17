7368299438b2d3a4bccef5a09c8954396d97de4d
rongchen0905@gmail.com <rcpenny@Chens-MacBook-Pro.local>
2020-01-16 21:58:46 -0800

    Maximum Depth of N-ary Tree N叉树的最大深度

---
+++ LC0500-0599/LC559.java
@@ -0,0 +1,18 @@
+// Maximum Depth of N-ary Tree N叉树的最大深度
+
+@EASY
+public class LC559 {
+
+  @Tree
+  @DivideConquer
+  public int maxDepth(Node root) {
+    if (root == null) return 0;
+
+    int max = 0;
+    for (Node child : root.children) {
+      max = Math.max(max, maxDepth(child));
+    }
+
+    return max + 1;
+  }
+}
