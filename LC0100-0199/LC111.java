b20f6c0823436b2ba6a33116432903b490215af7
Chen Rong <rcpenny@Chens-MacBook-Pro.local>
2020-01-16 18:32:34 -0800

    Minimum Depth of Binary Tree 二叉树的最小深度

---
+++ LC0100-0199/LC111.java
@@ -0,0 +1,21 @@
+// Minimum Depth of Binary Tree 二叉树的最小深度
+
+@EASY
+public class LC111 {
+  
+  @Tree
+  @DivideConquer
+  public int minDepth(TreeNode root) {
+    if (root == null) return 0;
+
+    int left = minDepth(root.left);
+    int right = minDepth(root.right);
+
+    if (root.left == null) return right + 1;
+    if (root.right == null) return left + 1;
+
+    return Math.min(left, right) + 1;  
+  }
+}
+
+
