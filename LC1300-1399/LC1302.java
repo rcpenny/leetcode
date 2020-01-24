41a5bf5a797860f16b09c2d2bee49296321d87d3
rcpenny <rongchen0905@gmail.com>
2020-01-23 22:47:32 -0800

    Deepest Leaves Sum 层数最深叶子节点的和

---
+++ LC1300-1399/LC1302.java
@@ -0,0 +1,30 @@
+// Deepest Leaves Sum 层数最深叶子节点的和
+
+@MEDIUM
+public class LC1302 {
+
+  int sum = 0;
+  int deepest = 1;
+  
+  @DivideConquer
+  @TopDown
+  public int deepestLeavesSum(TreeNode root) {
+    topDown(root, 1);
+    return sum;
+  }
+
+  public void topDown(TreeNode node, int curDepth) {
+    if (node == null) return;
+
+    if (curDepth == deepest) sum += node.val;
+    
+    if (curDepth > deepest) {
+      sum = 0;
+      deepest = curDepth;
+      sum += node.val;
+    }
+
+    topDown(node.left, curDepth + 1);
+    topDown(node.right, curDepth + 1);
+  }
+}
