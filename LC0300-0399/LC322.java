794bfe16ebd93d93f05d4310d858d1f98e06be6f
rcpenny <rongchen0905@gmail.com>
2020-01-25 23:11:31 -0800

    Coin Change 零钱兑换

--- LC0300-0399/LC322.java
+++ LC0300-0399/LC322.java
@@ -1,22 +1,16 @@
-// Coin Change
+// Coin Change 零钱兑换
 
 @MEDIUM
-@Google
 public class LC322 {
-  
+
   @DynamicProgramming
   public int coinChange(int[] coins, int amount) {
-    Arrays.sort(coins);
-
     int[] f = new int[amount + 1];
-    f[0] = 0;
 
-    int i, j;
-
-    for (i = 1; i <= amount; i++) {
+    for (int i = 1; i <= amount; i++) {
       f[i] = Integer.MAX_VALUE;
-      for (j = 0; j < coins.length; j++) {
-        int comp = i - coins[j];
+      for (int coin : coins) {
+        int comp = i - coin;
         if (comp >= 0 && f[comp] != Integer.MAX_VALUE) {
           f[i] = Math.min(f[i], f[comp] + 1);
         }
