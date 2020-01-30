d0046d6a54c9abec20cd7aefff342083018a8e2b
rcpenny <rongchen0905@gmail.com>
2020-01-29 21:09:49 -0800

    Same Tree 相同的树

---
+++ LC0100-0199/LC100.java
@@ -0,0 +1,15 @@
+// Same Tree 相同的树
+
+@EASY
+public class LC100 {
+
+  @DivideConquer
+  @BottomUp
+  public boolean isSameTree(TreeNode p, TreeNode q) {
+    if (p == null && q == null) return true;
+    if (p == null || q == null) return false;
+    if (p.val != q.val) return false;
+
+    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);  
+  }
+}
