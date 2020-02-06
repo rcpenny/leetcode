b1b7c1296b966f5d3c32d9b2aed74b09981ad933
rcpenny <rongchen0905@gmail.com>
2020-02-06 00:21:20 -0800

    Third Maximum Number 第三大的数

---
+++ LC0400-0499/LC414.java
@@ -0,0 +1,28 @@
+// Third Maximum Number 第三大的数
+
+@EASY
+public class LC414 {
+  private long MIN = Long.MIN_VALUE;
+
+  @Array
+  public int thirdMax(int[] nums) {
+    long larger = MIN;
+    long middle = MIN;
+    long smaller = MIN;
+
+    for (int n : nums) {
+      if (n > larger) {
+        smaller = middle;
+        middle = larger;
+        larger = n;
+      } else if (n < larger && n > middle) {
+        smaller = middle;
+        middle = n;
+      } else if (n < middle && n > smaller) {
+        smaller = n;
+      }
+    }
+
+    return smaller == Long.MIN_VALUE ? (int) larger : (int) smaller;
+  }
+}
