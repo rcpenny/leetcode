1963377ba52b3d56fe3d30493536115bf1cd8ab9
Chen Rong <crong@google.com>
2020-02-11 16:37:25 -0800

    Remove Duplicates from Sorted Array 删除排序数组中的重复项

--- LC0001-0099/LC26.java
+++ LC0001-0099/LC26.java
@@ -1,24 +1,22 @@
-// Remove Duplicates from Sorted Array
+// Remove Duplicates from Sorted Array 删除排序数组中的重复项
 
 @EASY
 public class LC26 {
 
   @TwoPointers
   public int removeDuplicates(int[] nums) {
-    if (nums.length == 0) return 0;
-    
-    int fast = 1;
-    int slow = 1;
+    int fast = 0;
+    int slow = 0;
 
     while (fast < nums.length) {
-      if (nums[fast] == nums[fast - 1]) {
+      if (nums[fast] == nums[slow]) {
         fast++;
         continue;
       }
 
-      nums[slow++] = nums[fast++];
+      nums[++slow] = nums[fast++];
     }
-
-    return slow;
+      
+    return slow + 1;
   }
 }
