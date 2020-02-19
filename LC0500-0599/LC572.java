3d15676203af44a19327f5831d1e1becdad8b0a1
Chen Rong <crong@google.com>
2020-02-19 14:37:28 -0800

    Subtree of Another Tree 另一个树的子树

---
+++ LC0500-0599/LC572.java
@@ -0,0 +1,23 @@
+// Subtree of Another Tree 另一个树的子树
+
+@EASY
+public class LC572 {
+
+	@Tree
+	@DivideConquer
+	public boolean isSubtree(TreeNode s, TreeNode t) {
+    if (sameTree(s, t)) return true;
+
+    boolean left = s.left != null ? isSubtree(s.left, t) : false;
+    boolean right = s.right != null ? isSubtree(s.right, t) : false;
+
+    return left || right;
+  }
+
+  private boolean sameTree(TreeNode a, TreeNode b) {
+    if (a == null && b == null) return true;
+    if (a == null || b == null) return false;
+
+    return a.val == b.val && sameTree(a.left, b.left) && sameTree(a.right, b.right);
+  }
+}
