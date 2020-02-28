84cd6c565941ffa080783eb160f1271320231782
Chen Rong <crong@google.com>
2020-02-28 15:47:54 -0800

    Meeting Rooms 会议室

--- LC0200-0299/LC252.java
+++ LC0200-0299/LC252.java
@@ -3,24 +3,23 @@
 @EASY
 public class LC252 {
 
-  @Heap
+	// O(nLogn) O(1)
+  @Sorting
   public boolean canAttendMeetings(List<Interval> intervals) {
     if (intervals == null || intervals.size() <= 1) return true;
 
 		Collections.sort(intervals, (a, b) -> {
-			if (a.start != b.start) return a.start - b.start;
-			return a.end - b.end;
+			return a.start != b.start ? a.start - b.start : a.end - b.end;
 		});
 
-    for (int i = 0; i < intervals.size() - 1; i++) {
-      Interval current = intervals.get(i);
-      Interval next = intervals.get(i + 1);
-      if (current.end > next.start) return false;
-    }
+    for (int i = 0; i < intervals.size() - 1; i++)
+      if (intervals.get(i).end > intervals.get(i + 1).start)
+				return false;
 
     return true;
 	}
-	
+
+	// O(n) O(n)
   @SweepLine
 	public boolean canAttendMeetings2(List<Interval> intervals) {
 		if (intervals == null || intervals.size() <= 1) return true;
