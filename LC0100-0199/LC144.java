152cbe61c3f464daa673256cb5d4bc875b9597b2
Chen Rong <crong@google.com>
2020-03-03 15:46:28 -0800

    Binary Tree Preorder Traversal 二叉树的前序遍历

--- LC0100-0199/LC144.java
+++ LC0100-0199/LC144.java
@@ -20,8 +20,6 @@ public class LC144 {
   }
 
 
-	/** 迭代解法本质上是在模拟递归，因为在递归的过程中使用了系统栈，所以在迭代的解法中常用Stack来模拟系统栈 */
-
 	// 前序遍历： 中 左 右 （出栈顺序）
   @Iterative
   public List<Integer> preorderTraversal(TreeNode root) {
