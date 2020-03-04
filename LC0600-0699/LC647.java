8f7657346f3e0e147328b974fbbfa1c359c6c45c
Chen Rong <crong@google.com>
2020-03-04 13:53:30 -0800

    Palindromic Substrings 回文子串

---
+++ LC0600-0699/LC647.java
@@ -0,0 +1,41 @@
+// Palindromic Substrings 回文子串
+
+@MEDIUM
+public class LC647 {
+  
+	@DynamicProgramming
+	public int countPalindromicSubstrings(String str) {
+    if (str == null) return 0;
+
+    int n = str.length();
+    if (n <= 1) return n;
+
+    char[] c = str.toCharArray();
+    boolean[][] f = new boolean[n][n];
+
+    int i, j, len;
+
+    // len = 1
+    for (i = 0; i < n; i++)
+      f[i][i] = true;
+    
+    // len = 2
+    for (i = 0; i < n - 1; i++)
+      f[i][i + 1] = c[i] == c[i + 1] ? true : false;
+
+    // 序列型 根据length来循环
+    for (len = 3; len <= n; len++) {
+      for (i = 0; i <= n - len; i++) {
+        j = i + len - 1;
+        f[i][j] = f[i + 1][j - 1] && (c[i] == c[j]);
+      }
+    }
+
+    int count = 0;
+    for (i = 0; i < n; i++)
+      for (j = 0; j < n; j++)
+        if (f[i][j]) count++;
+    
+    return count;
+  }
+}
