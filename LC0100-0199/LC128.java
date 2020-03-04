223f9ae8e7966791fa5f15190919778409d96e86
Chen Rong <crong@google.com>
2020-03-04 13:59:43 -0800

    Longest Consecutive Sequence 最长连续序列

--- LC0100-0199/LC128.java
+++ LC0100-0199/LC128.java
@@ -2,43 +2,37 @@
 
 @HARD
 public class LC128 {
-	
-	@Array
-	@HashSet
-	public int longestConsecutive(int[] nums) {
+
+  @HashSet
+  @UnionFind
+  public int longestConsecutive(int[] nums) {
     if (nums == null || nums.length == 0) return 0;
 
-    int max = 1;
-
     Set<Integer> set = new HashSet<>();
+    
     for (int n : nums) set.add(n);
+    int longest = 1;
 
-    for (int n : nums) {
-      if (!set.contains(n)) continue;
-      set.remove(n);
-      max = Math.max(max, longestConsec(set, n));
+    for (int i = 0; i < nums.length; i++) {
+      if (!set.contains(nums[i])) continue;
+
+      int left = nums[i] - 1;
+      int right = nums[i] + 1;
+      int len = 1;
+
+      while (set.contains(left)) {
+        len++;
+        set.remove(left--);
+      }
+
+      while (set.contains(right)) {
+        len++;
+        set.remove(right++);
+      }
+
+      longest = Math.max(longest, len);
     }
 
-    return max;
-  }
-
-  private int longestConsec(Set<Integer> set, int number) {
-    int len = 1;
-    int left = number - 1;
-    int right = number + 1;
-
-    while (set.contains(left)) {
-      len++;
-      set.remove(left);
-      left--;
-    }
-
-    while (set.contains(right)) {
-      len++;
-      set.remove(right);
-      right++;
-    }
-
-    return len;
+    return longest;
   }
 }
