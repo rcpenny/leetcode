810076466918201e07c71d59887c1daefb750ca3
Chen Rong <crong@google.com>
2020-03-04 13:00:36 -0800

    Binary Tree Upside Down 上下翻转二叉树

---
+++ LC0100-0199/LC156.java
@@ -0,0 +1,25 @@
+// Binary Tree Upside Down 上下翻转二叉树 
+
+@MEDIUM
+public class LC156 {
+
+	@Recursion
+	public TreeNode upsideDownBinaryTree(TreeNode root) {
+		return helper(null, root);
+	}
+
+	private TreeNode helper(TreeNode parent, TreeNode node) {
+		if (node == null) return parent; // 得到新的root node
+
+		TreeNode newRoot = helper(node, node.left);
+
+		if (parent != null) {
+			node.left = parent.right;
+		} else {
+			node.left = null;
+		}
+		node.right = parent;
+
+		return newRoot;
+	}
+}
