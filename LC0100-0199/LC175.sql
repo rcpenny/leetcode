52f4209d535f8c270a44ba7469641e41447a850a
rcpenny <rongchen0905@gmail.com>
2020-03-07 21:58:54 -0800

    Combine Two Tables 组合两个表

--- LC0100-0199/LC175.sql
+++ LC0100-0199/LC175.sql
@@ -1,3 +1,9 @@
-SELECT p.FirstName, p.LastName, a.City, a.State
-FROM Person p LEFT JOIN Address a
-ON p.PersonId = a.PersonId;
+/* Combine Two Tables 组合两个表 */
+
+SELECT 
+  p.FirstName, p.LastName,
+  a.City, a.State
+FROM
+  Person p LEFT JOIN Address a
+ON 
+  p.PersonId = a.PersonId;
