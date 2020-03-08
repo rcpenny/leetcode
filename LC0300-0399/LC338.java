f25506c0a7ade08fb0cf440794e0910e66948fbc
rcpenny <rongchen0905@gmail.com>
2020-03-08 15:05:32 -0700

    Counting Bits 比特位计数

---
+++ LC0300-0399/LC338.java
@@ -0,0 +1,16 @@
+// Counting Bits 比特位计数
+
+@MEDIUM
+public class LC338 {
+
+	@DynamicProgramming
+  public int[] countBits(int num) {
+    int[] bits = new int[num + 1];
+    bits[0] = 0;
+
+    for (int i = 1; i <= num; i++)
+      bits[i] = bits[i / 2] + i % 2;
+
+    return bits;
+  }
+}
