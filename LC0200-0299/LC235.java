726be1923d4f6743e2b3c2cdc8bf44cacf90a616
rcpenny <rongchen0905@gmail.com>
2020-03-08 19:53:21 -0700

    Lowest Common Ancestor of a Binary Search Tree 二叉搜索树的最近公共祖先

--- LC0200-0299/LC235.java
+++ LC0200-0299/LC235.java
@@ -1,19 +1,21 @@
-// Lowest Common Ancestor of a Binary Search Tree
+// Lowest Common Ancestor of a Binary Search Tree 二叉搜索树的最近公共祖先
 
 @EASY
-@Microsoft
 class LC235 {
 
-  @BST  
+  @TopDown
   public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-    if (root.val > p.val && root.val > q.val) {
+    if (root.val > p.val && root.val > q.val)
       return lowestCommonAncestor(root.left, p, q);
-    }
 
-    if (root.val < p.val && root.val < q.val) {
+    if (root.val < p.val && root.val < q.val)
       return lowestCommonAncestor(root.right, p, q);
-    }
 
     return root;
   }
 }
+
+/**
+ * 思路：
+ * ancestor一定是介于pq之间的值，根据BST性质左右摇摆
+ */
