e2152a8a8440f526cabf42290bdfafb95e298380
Chen Rong <crong@google.com>
2020-03-09 13:40:02 -0700

    Longest Univalue Path 最长同值路径

---
+++ LC0600-0699/LC687.java
@@ -0,0 +1,33 @@
+// Longest Univalue Path 最长同值路径
+
+@EASY
+public class LC687 {
+  int result;
+
+  public int longestUnivaluePath(TreeNode root) {
+      result = 0;
+      arrowLength(root);
+      return result;
+  }
+  
+  public int arrowLength(TreeNode node) {
+    if (node == null) return 0;
+    
+    int left = arrowLength(node.left);
+    int right = arrowLength(node.right);
+    
+    int arrowLeft = 0;
+    int arrowRight = 0;
+
+    if (node.left != null && node.left.val == node.val) {
+      arrowLeft += left + 1;
+    }
+    
+    if (node.right != null && node.right.val == node.val) {
+      arrowRight += right + 1;
+    }
+    
+    result = Math.max(result, arrowLeft + arrowRight);
+    return Math.max(arrowLeft, arrowRight);
+  }
+}
