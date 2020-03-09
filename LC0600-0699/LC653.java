a2ac6beaa9f0b30f2f38ca4268245d4216063515
Chen Rong <crong@google.com>
2020-03-09 12:23:34 -0700

    Two Sum IV - Input is a BST 两数之和 IV - 输入 BST

--- LC0600-0699/LC653.java
+++ LC0600-0699/LC653.java
@@ -1,8 +1,29 @@
-// Two Sum IV - Input is a BST
+// Two Sum IV - Input is a BST 两数之和 IV - 输入 BST
 
+@EASY
 public class LC653 {
+  boolean exist = false;
+  Set<Integer> set;
 
-  private LC653() {
+	@RecursionTraversal
+  public boolean findTarget(TreeNode root, int k) {
+    set = new HashSet<>();
+    traverse(root, k);
+    return exist;
   }
 
+  private void traverse(TreeNode node, int k) {
+    if (exist || node == null) return;
+
+    traverse(node.left, k);
+    System.out.println(node.val);
+
+    int comp = k - node.val;    
+    if (set.contains(comp)) exist = true;
+    set.add(node.val);
+
+    traverse(node.right, k);
+  }
 }
+
+// 其实此题目的是LC1214
