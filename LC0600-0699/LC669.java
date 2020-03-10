601faa4a31bfde0a7ec7b07ab797f064ce515b4d
rcpenny <rongchen0905@gmail.com>
2020-03-09 23:58:32 -0700

    Trim a Binary Search Tree 修剪二叉搜索树

---
+++ LC0600-0699/LC669.java
@@ -0,0 +1,17 @@
+// Trim a Binary Search Tree 修剪二叉搜索树
+
+@EASY
+public class LC669 {
+
+	@BST
+  public TreeNode trimBST(TreeNode root, int L, int R) {
+    if (root == null) return null;
+
+    if (root.val < L) return trimBST(root.right, L, R);
+    else if (root.val > R) return trimBST(root.left, L, R);
+
+    root.left = trimBST(root.left, L, R);
+    root.right = trimBST(root.right, L, R);
+    return root;
+  }
+}
