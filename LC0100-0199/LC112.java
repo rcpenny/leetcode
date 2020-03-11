a3c2045b3e3102a1f6ef0339cbab3caad488a1c7
Chen Rong <crong@google.com>
2020-03-11 16:49:28 -0700

    Path Sum 路径总和

--- LC0100-0199/LC112.java
+++ LC0100-0199/LC112.java
@@ -3,19 +3,14 @@
 @EASY
 public class LC112 {
 
-  @DivideConquer
   @TopDown
   public boolean hasPathSum(TreeNode root, int sum) {
     if (root == null) return false;
+    if (root.left == null && root.right == null) return root.val == sum;
 
-    if (root.left == null && root.right == null) {
-      if (root.val == sum) return true;
-      return false;
-    }
+    boolean leftExists = hasPathSum(root.left, sum - root.val);
+    boolean rightExists = hasPathSum(root.right, sum - root.val);
 
-    boolean leftResult = hasPathSum(root.left, sum - root.val);
-    boolean rightResult = hasPathSum(root.right, sum - root.val);
-
-    return leftResult || rightResult;
+    return leftExists || rightExists;
   }
 }
