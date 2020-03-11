963d120cfcfa7104aa0af9ca379c167c59bb864e
Chen Rong <crong@google.com>
2020-03-11 15:48:21 -0700

    Fibonacci Number 斐波那契数

---
+++ LC0500-0599/LC509.java
@@ -0,0 +1,20 @@
+// Fibonacci Number 斐波那契数
+
+@EASY
+public class LC509 {
+
+	@DP
+  public int fib(int N) {
+    if (N <= 1) return N;
+
+    int[] f = new int[N + 1];
+    
+    f[0] = 0;
+    f[1] = 1;
+
+    for (int i = 2; i <= N; i++)
+      f[i] = f[i - 1] + f[i - 2];
+    
+    return f[N];
+  }
+}
