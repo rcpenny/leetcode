2cacfcb5541e5a70f2af028ccf726542aee17ca2
Chen Rong <crong@google.com>
2020-03-11 16:58:06 -0700

    Flip Equivalent Binary Trees 翻转等价二叉树

--- LC0900-0999/LC951.java
+++ LC0900-0999/LC951.java
@@ -1,15 +1,13 @@
-// Flip Equivalent Binary Trees
+// Flip Equivalent Binary Trees 翻转等价二叉树
 
 @MEDIUM
-@Google
 public class LC951 {
 
-  @DivideConquer
-  @TopDown
+  @BottomUp
   public boolean flipEquiv(TreeNode root1, TreeNode root2) {
     if (root1 == null && root2 == null) return true;
-    if (root1 == null || root2 == null) return false;
-
+    
+		if (root1 == null || root2 == null) return false;
     if (root1. val != root2.val) return false;
 
     boolean noflip = flipEquiv(root1.left, root2.left) && flipEquiv(root1.right, root2.right);
