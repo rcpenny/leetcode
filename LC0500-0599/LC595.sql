9e059f32a3072459aef6397c044776ae784d8006
Chen Rong <crong@google.com>
2020-03-12 10:55:06 -0700

    Big Country 大的国家

--- LC0500-0599/LC595.sql
+++ LC0500-0599/LC595.sql
@@ -1,3 +1,10 @@
-SELECT w.name, w.population, w.area
-FROM World w
-WHERE w.population > 25000000 OR w.area > 3000000;
+SELECT
+  w.name,
+  w.population,
+  w.area
+FROM
+  World w
+WHERE 
+  w.population > 25000000 
+  OR 
+  w.area > 3000000;
