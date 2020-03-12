466c44815edee119cbe8c37515346f9704a86679
rcpenny <rongchen0905@gmail.com>
2020-03-11 23:01:38 -0700

    Two City Scheduling 两地调度

---
+++ LC1000-1099/LC1072.java
@@ -0,0 +1,23 @@
+// Two City Scheduling 两地调度
+
+@EASY
+public class LC1072 {
+	
+	@Greedy
+	public int twoCitySchedCost(int[][] costs) {
+    int totalCost = 0;
+
+    for (int i = 0; i < costs.length; i++)
+      totalCost += costs[i][0];
+    
+    Arrays.sort(costs, (a, b) -> {
+      return (a[1] - a[0]) - (b[1] - b[0]);
+    });
+
+    for (int i = 0; i < costs.length / 2; i++) {
+      totalCost += (costs[i][1] - costs[i][0]);
+    }
+
+    return totalCost;
+  }
+}
