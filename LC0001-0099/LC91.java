769bb04af2f66439f28d70e048ba40909868244b
Chen Rong <crong@google.com>
2020-03-12 10:06:08 -0700

    Decode Ways 解码方法

--- LC0001-0099/LC91.java
+++ LC0001-0099/LC91.java
@@ -1,25 +1,23 @@
-// Decode Ways
+// Decode Ways 解码方法
 
 @MEDIUM
 public class LC91 {
 
-  @DynamicProgramming
+  @DP
   public int numDecodings(String s) {
 		if (s == null || s.length() == 0) return 0;
 
-		char[] array = s.toCharArray();
+		char[] letters = s.toCharArray();
 		int n = s.length();
 		int[] f = new int[n + 1];
 
 		f[0] = 1;
 
 		for (int i = 1; i <= n; i++) {
-			if (array[i - 1] != '0') {
-				f[i] += f[i - 1];
-			}
+			if (letters[i - 1] != '0') f[i] += f[i - 1];
 
 			if (i > 1) {
-				if (array[i - 2] == '1' || (array[i - 2] == '2' && array[i - 1] <= '6'))
+				if (letters[i - 2] == '1' || (letters[i - 2] == '2' && letters[i - 1] <= '6'))
 				  f[i] += f[i - 2];
 			}
 		}
