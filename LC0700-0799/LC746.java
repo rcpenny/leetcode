7093358e76f65345107a0ef526b0d8fcfe9ae157
Chen Rong <crong@google.com>
2020-03-12 10:23:38 -0700

    Min Cost Climbing Stairs 使用最小花费爬楼梯

--- LC0700-0799/LC746.java
+++ LC0700-0799/LC746.java
@@ -3,7 +3,7 @@
 @EASY
 public class LC746 {
 
-	@DynamicProgramming
+	@DP
   public int minCostClimbingStairs(int[] cost) {
     int n = cost.length;
     int[] f = new int[n + 1];
@@ -11,9 +11,8 @@ public class LC746 {
      f[0] = 0;
      f[1] = 0;
 
-    for (int i = 2; i <= n; i++) {
+    for (int i = 2; i <= n; i++)
       f[i] = Math.min(f[i - 1] + cost[i - 1], f[i - 2] + cost[i - 2]);
-    }
 
     return f[n];
   }
