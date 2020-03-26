d45cbd9cb80f4d1bed22953061284616225733ef
rcpenny <rongchen0905@gmail.com>
2020-03-26 21:01:41 +0800

    Move Zeros 移动零

--- LC0200-0299/LC283.java
+++ LC0200-0299/LC283.java
@@ -6,20 +6,20 @@ public class LC283 {
 	@Array
 	public void moveZeroes(int[] nums) {
 		int countZeros = 0;
-
-		for (int i : nums) {
-			if (i == 0)
-				countZeros++;
 		
-		int curIndex = 0;
-
-		for (int i = 0; i < nums.length; i++) {
-			if (nums[i] == 0) continue;
-			nums[curIndex] = nums[i];
-			curIndex++;
+		for (int i : nums) {
+			if (i == 0) countZeros++;
 		}
 		
-		for (int j = 0; j < countZeros; j++)
+		int curIndex = 0;
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] != 0) {
+				nums[curIndex++] = nums[i];
+			}
+		}
+		
+		for (int j = 0; j < countZeros; j++) {
 			nums[nums.length - 1 - j] = 0;
+		}
 	}
 }
