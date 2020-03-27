2f50b9e9efe8b15ce41398f11ab4db39f82ecefa
rcpenny <rongchen0905@gmail.com>
2020-03-27 11:53:18 +0800

    Product of Array Except Self 除自身以外数组的乘积

--- LC0200-0299/LC238.java
+++ LC0200-0299/LC238.java
@@ -12,13 +12,13 @@ public class LC238 {
     
     for (int i = 0; i < nums.length; i++) {
       result[i] = mul;
-      mul = nums[i] * mul;
+      mul *= nums[i];
     }
 
     mul = 1;
     for (int i = nums.length - 1; i >= 0; i--) {
       result[i] = mul * result[i];
-      mul = mul * nums[i];
+      mul *= nums[i];
     }
     
     return result;
