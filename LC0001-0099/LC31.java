30d8398b7d53ffcff0e4eb6b6d2d4c0d19456956
rcpenny <rongchen0905@gmail.com>
2020-03-27 11:48:33 +0800

    Next Permutation 下一个排列

--- LC0001-0099/LC31.java
+++ LC0001-0099/LC31.java
@@ -1,31 +1,28 @@
 // Next Permutation 下一个排列
 
 @MEDIUM
+@HighFreq
 public class LC31 {
 
   @Array
   public void nextPermutation(int[] nums) {
     int i = nums.length - 2;
-    while (i >= 0 && nums[i + 1] <= nums[i]) {
-      i--;
-    }
+    while (i >= 0 && nums[i + 1] <= nums[i]) i--;
 
     if (i >= 0) {
       int j = nums.length - 1;
-      while (j >= 0 && nums[j] <= nums[i]) {
-        j--;
-      }
+      while (j >= 0 && nums[j] <= nums[i]) j--;
       swap(nums, i, j);
     }
+
     reverse(nums, i + 1);
   }
 
   private void reverse(int[] nums, int start) {
     int left = start;
     int right = nums.length - 1;
-    while (left < right) {
-      swap(nums, left++, right--);
-    }
+    
+		while (left < right) swap(nums, left++, right--);
   }
 
   private void swap(int[] nums, int i, int j) {
