ddea952267ec899fece86192ef0c7fce46735640
rcpenny <rongchen0905@gmail.com>
2020-04-02 17:03:20 +0800

    字符串中的第一个唯一字符 First Unique Character in a String

---
+++ LC0300-0399/LC387.java
@@ -0,0 +1,19 @@
+// 字符串中的第一个唯一字符 First Unique Character in a String
+
+public class LC387 {
+
+	@HashTable
+	public int firstUniqChar(String s) {
+		HashMap<Character, Integer> map = new HashMap<>();
+		int n = s.length();
+
+		for (char c : s.toCharArray())
+			map.put(c, map.getOrDefault(c, 0) + 1);
+		
+		for (int i = 0; i < n; i++)
+			if (map.get(s.charAt(i)) == 1) 
+				return i;
+
+		return -1;
+	}
+}
